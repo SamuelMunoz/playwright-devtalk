43e5e0acb062fc07fa6af1cee6124ae54149dffa
Samuel Munoz <sdmunoz@gmail.com>
2023-03-01 17:32:23 -0600

    feat: code start

---
+++ Playwright-Demo/Tests.cs
@@ -0,0 +1,16 @@
+﻿using System.Text.RegularExpressions;
+using Microsoft.Playwright.NUnit;
+
+namespace Playwright_Demo;
+
+[Parallelizable(ParallelScope.Self)]
+[TestFixture]
+public class Tests : PageTest
+{
+    [Test]
+    public async Task HomePageHasGoogleInItsTitle()
+    {
+        await Page.GotoAsync("https://google.com");
+        await Expect(Page).ToHaveTitleAsync(new Regex("Google"));
+    }
+}
